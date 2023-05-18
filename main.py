d9b7f9f74f15cae9452473e4bfff70549fa7b2ba
student <nikitamendukov@gmail.com>
2023-05-18 20:17:32 +0300

    замикання

--- main.py
+++ main.py
@@ -25,3 +25,22 @@ def my_generator(data):
 
 for num in my_generator(lst):
     print(num)
+print()
+#замикання
+
+def Calculator():
+    def add(a, b):
+        return a + b
+    def sub(a, b):
+        return a - b
+    def mult(a, b):
+        return a * b
+    def div(a, b):
+        if b != 0:
+            return a / b
+        else:
+            raise ValueError('Не можна ділити на нуль!')
+    return add, sub, mult, div
+add, sub, mult, div = Calculator()
+print(int(div(3, 1)))
+
