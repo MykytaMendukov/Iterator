440c29187b165c79de4bc9024f2cccac4a34baa3
student <nikitamendukov@gmail.com>
2023-05-23 19:13:54 +0300

    factorial

--- logging_23.05.py
+++ logging_23.05.py
@@ -9,4 +9,20 @@ logging.info('info')
 logging.warning('warming')
 logging.error('error')
 logging.critical('critical')
+try:
+    print(10 / 0)
+except Exception:
+    logging.exception('We have a problem!')
 
+
+#факторіал числа
+
+def factorial(n):
+    logging.info(f'Розпочато обчислення факторіалу числа {n}')
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    logging.info(f'Обчислення факторіалу числа {n} завершено. Результат виконання: {result}')
+    return result
+logging.basicConfig(level=logging.INFO)
+factorial(5)
