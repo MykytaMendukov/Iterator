3865a9107653d253b83fa9280ebaaba5fdd2d24c
student <nikitamendukov@gmail.com>
2023-05-23 18:56:00 +0300

    запис в .txt

--- logging_23.05.py
+++ logging_23.05.py
@@ -1,6 +1,9 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')  #щоб визвати лог, як пріоритетний
+logging.basicConfig(level=logging.DEBUG,  #щоб визвати лог, який не є помилкою
+                    filename= 'logs.log',
+                    filemode= 'w',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug('debug')
 logging.info('info')
 logging.warning('warming')
